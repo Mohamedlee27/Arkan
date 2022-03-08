54692cb1ffd1007818356f37d028cda8a455d854
Mohamedlee27 <leonk8070@gmail.com>
2022-03-08 13:29:01 -0800

    added akan name variable

--- arkan.js
+++ arkan.js
@@ -4,6 +4,12 @@ let date = document.getElementById('date').value;
 let maleGender = document.getElementById('male').value;
 let femaleGender = document.getElementById('female').value;
 let submit = document.getElementById('submit');
+let akanName = document.getElementById('name');
 
 let birthday = new Date('date');
 let dayOfWeek = birthday.getDay();
+
+if(maleGender){
+
+
+}
