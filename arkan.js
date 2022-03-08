fd60d20677cfeef85ea59bb85bd670a707be280d
Mohamedlee27 <leonk8070@gmail.com>
2022-03-08 14:01:06 -0800

    added another if function for female

--- arkan.js
+++ arkan.js
@@ -1,17 +1,25 @@
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-let date = document.getElementById('date').value;
-let maleGender = document.getElementById('male').value;
-let femaleGender = document.getElementById('female').value;
 let submit = document.getElementById('submit');
-let akanName = document.getElementById('name');
+submit.addEventListener('click', function(){
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let date = document.getElementById('date').value;
+    let maleGender = document.getElementById('male').value;
+    let femaleGender = document.getElementById('female').value;
 
-let birthday = new Date('date');
-let dayOfWeek = birthday.getDay();
+    let akanName = document.getElementById('name');
+
+    let birthday = date;
+let dateOfBirth = new Date(birthday);
+let dayOfWeek = dateOfBirth.getDay();
+
+console.log(dayOfWeek);
 
 if(maleGender){
     akanName.innerHTML = "Your name is:" + maleNames[dayOfWeek];
-    
+
 }
 
-
+if(femaleGender){
+    akanName.innerHTML = "Your name is: " + femaleNames[dayOfWeek];
+}
+});
