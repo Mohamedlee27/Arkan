daea645a63c1dbbcefac377ac77196b1e90160e1
Mohamedlee27 <leonk8070@gmail.com>
2022-03-08 13:21:27 -0800

    edited the date variable

--- arkan.js
+++ arkan.js
@@ -3,3 +3,6 @@ let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let date = document.getElementById('date').value;
 let maleGender = document.getElementById('male').value;
 let femaleGender = document.getElementById('female').value;
+let submit = document.getElementById('submit');
+
+let birthday = new Date('date');
