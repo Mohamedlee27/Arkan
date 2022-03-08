76c519fc9bbb5f5e93e3108caf87bc93a1e3a674
Mohamedlee27 <leonk8070@gmail.com>
2022-03-08 13:14:25 -0800

    added female and male variables

--- arkan.js
+++ arkan.js
@@ -1,3 +1,5 @@
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let date = document.getElementById('date').value;
+let maleGender = document.getElementById('male').value;
+let femaleGender = document.getElementById('female').value;
